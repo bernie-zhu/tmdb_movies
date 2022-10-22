91f20b8a88a37df29d3f85f0300dc0438613fa48
bernie-zhu <bwz2@illinois.edu>
2022-10-21 22:57:28 -0500

    hash router round 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Details from "./components/Details/Details"
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Header/>
       <Navigation/>
       <Routes>
@@ -16,7 +16,7 @@ function App() {
         <Route path="/list" element={<List/>} />
         <Route path="/details/:passedId" element={<Details/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
